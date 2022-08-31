81cd4215e229ecde73f0fb91ccde6aa489a8c177
Noah Wang <purelyblank2018@gmail.com>
2022-08-30 21:52:15 -0700

    Updates on cogs command

--- cogs/commands.py
+++ cogs/commands.py
@@ -3,3 +3,17 @@ from discord.ext import commands
 
 # use cog for all commands
 # maybe use inheritence for different grouping of commands
+
+class Commands(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        await ctx.send(f'Ping: {round(self.client.latency * 1000)}ms')
+
+
+async def setup(client):
+    await client.add_cog(Commands(client))
